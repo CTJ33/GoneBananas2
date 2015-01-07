33b2ae131a1a8ab547d53b513ba3da42ab10da51
Johnson <johnct17@eamac3411.local>
2015-01-07 11:44:48 -0500

    Initial Commit

---
+++ GoneBananas2/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  GoneBananas2
+//
+//  Created by Johnson, ColtonTanner on 1/7/15.
+//  Copyright (c) 2015 ColtonJ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ GoneBananas2/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  GoneBananas2
+//
+//  Created by Johnson, ColtonTanner on 1/7/15.
+//  Copyright (c) 2015 ColtonJ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
